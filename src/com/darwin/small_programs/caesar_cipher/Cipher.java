48a899a566faefda23e42cf527b3fc9cf6eaec0e
darwin1501 <darwin.pablo1501@gmail.com>
2021-12-28 17:02:16 +0800

    add encryption

--- src/com/darwin/small_programs/caesar_cipher/Cipher.java
+++ src/com/darwin/small_programs/caesar_cipher/Cipher.java
@@ -2,6 +2,7 @@ package com.darwin.small_programs.caesar_cipher;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,13 +11,15 @@ public class Cipher {
 
     private File file;
 
-    char[] pattern = {
+    private Character[] pattern = {
             'a','b','c','d','e',
             'f','g','h','i','j',
             'k','l','m','n','o',
             'p','q','r','s','t','u',
             'v','w','x','y','z'
     };
+
+    private int shift = 3;
 
     public ArrayList<String> getFileToEncrypt(){
         file = new File("src/com/darwin/small_programs/caesar_cipher/text_files/encrypt_this.txt");
@@ -36,16 +39,83 @@ public class Cipher {
     public void encrypt(ArrayList<String> textList){
 
         ArrayList<ArrayList<Character>> charList = new ArrayList<>();
+        ArrayList<ArrayList<Character>> encryptedCharList = new ArrayList<>();
 
+//        create 2D array list of characters
         for (int i = 0; i < textList.size() ; i++) {
             ArrayList<Character> characters = new ArrayList<>();
             for (int j = 0; j < textList.get(i).length() ; j++) {
-//                System.out.println(textList.get(i).charAt(j));
                 characters.add(textList.get(i).charAt(j));
             }
             charList.add(characters);
         }
-
+//    print character list
         System.out.println(charList);
+
+        // get each character in 2d array list
+        for (int i = 0; i < charList.size() ; i++) {
+            ArrayList<Character> encryptedChar = new ArrayList<>();
+            for (int j = 0; j <charList.get(i).size(); j++) {
+                char character = charList.get(i).get(j);
+                int getIndex;
+                boolean isUpperCase = Character.isUpperCase(character);
+        // find character in the pattern array
+                getIndex = Arrays.asList(pattern).indexOf(character);
+
+        // if the program can't find the character in the pattern array,
+        // because the letter is in lower case
+                if (getIndex == -1){
+        // transform the character into lower case if this is a letter then find again
+                    getIndex = Arrays.asList(pattern).indexOf(Character.toLowerCase(character));
+        //  if the program can't find the character
+                    if (getIndex == -1){
+        // the character is not a letter
+                        System.out.println("Not a letter");
+        // store the characters in array list
+                        encryptedChar.add(character);
+                    }else{
+        // store the characters in array list
+                        encryptedChar.add(addShiftToLetters(getIndex, isUpperCase));
+                    }
+                }else{
+        // store the characters in array list
+                        encryptedChar.add(addShiftToLetters(getIndex, isUpperCase));
+                }
+            }
+        // store the character array list into array list
+            encryptedCharList.add(encryptedChar);
+        }
+//        print the result
+        System.out.println(encryptedCharList);
+    }
+//    replace the old letter to caesar letter alphabet by adding 3 to each letter
+    public char addShiftToLetters(int indexOfLetter, boolean isUpperCase){
+        char shiftedLetter = 0;
+//        add 3 to the letter
+//        e.g A + 3 = D
+        int letterIndex = indexOfLetter + shift;
+
+        try{
+            shiftedLetter = Arrays.asList(pattern).get(letterIndex);
+//       if the index exceed the alphabet length/letters
+        }catch (Exception e){
+//        then start
+//        at the beginning of the alphabet.
+//        e.g Y + 3 = B
+
+//          subtract the letterIndex to the total number of characters of the pattern then
+//          subtract by one again to get the exact index of the letter from the start of the alphabet
+//            e.g
+//                      Y will become B
+//              letterIndex(27) - alphabet indexes(25) = 2 is B
+            int remaining = letterIndex - (pattern.length - 1) -1;
+            shiftedLetter  = Arrays.asList(pattern).get(remaining);
+        }
+
+        if(isUpperCase){
+            return Character.toUpperCase(shiftedLetter);
+        }else{
+            return shiftedLetter;
+        }
     }
 }
