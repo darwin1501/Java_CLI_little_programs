72e164187f5b8c38eb240d5460159c20fb46ee9b
darwin1501 <darwin.pablo1501@gmail.com>
2022-05-02 08:10:00 +0800

    add challenge

---
+++ src/com/darwin/small_programs/candies/Candies.java
@@ -0,0 +1,25 @@
+package com.darwin.small_programs.candies;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Candies {
+    public void eat(){
+        Integer[] sugarOnCandies = { 33, 20, 12, 19, 29 };
+        int sugarLimit = 33;
+        int sugarConsumed = 0;
+//         candies consumed before reaching the limit
+        ArrayList<Integer> candiesConsumed = new ArrayList<>();
+
+        // sort the sugar on each candy
+        // add each sugar on candy
+        // find the index of candies
+        // if sugar limit reach then stop adding
+
+//        for (int i = 0; i < sugarOnCandies.length ; i++) {
+//
+//        }
+        System.out.println(candiesConsumed);
+    }
+}
